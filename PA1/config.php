9221b96792dc1c4e9ea358704854f107219fbbde
Johannes Sipayung <johannesssipayung27@gmail.com>
2023-06-18 23:32:49 +0700

    Domma Salosei

--- PA1/config.php
+++ PA1/config.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost:4306";
+    $servername = "localhost";
     $username = "root";
     $password = "";
-    $db_name = "mauasjkm";
+    $db_name = "mauasjkm1";
 
     $conn = mysqli_connect($servername,$username,$password,$db_name);
 ?> 
